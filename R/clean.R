d39d87a246cc939b1e5d8e0fc7c0ce34f9733789
Petr Kočí <petr.koci@samizdat.cz>
2018-02-08 12:41:57 +0100

    odstraněna vue

---
+++ R/clean.R
@@ -0,0 +1,17 @@
+library(readxl)
+library(jsonlite)
+
+# míra nezaměstnanosti - čištění
+mira_nezam <- read_excel("../data/mira-nezam-pohlavi-kvartalne-93-17.xlsx")
+mira_nezam <- t(mira_nezam)
+mira_nezam <- data.frame(mira_nezam)
+names(mira_nezam) <- c("celkem", "muži", "ženy")
+mira_nezam <- mira_nezam[-1,]
+mira_nezam$celkem <- as.numeric(as.character(mira_nezam$celkem))
+mira_nezam$muži <- as.numeric(as.character(mira_nezam$muži))
+mira_nezam$ženy <- as.numeric(as.character(mira_nezam$ženy))
+mira_nezam$období <- row.names(mira_nezam)
+row.names(mira_nezam) <- c()
+names(mira_nezam) <- c("c", "m", "z", "o")
+
+toJSON(mira_nezam)
---
+++ snowfall-builder/checklist.md
@@ -0,0 +1,17 @@
+# Zkontrolovat před vydáním článku
+- jak to vypadá na mobilu?
+- je správně uvedené datum publikace?
+- nestahují se zbytečně obří fotky?
+- fungují sdílecí tlačítka?
+- má článek mezi klíčovými slovy Datovou žurnalistiku?
+- má nastavené související?
+
+# Časté problémy a jejich řešení
+
+## Úvodní obrázek je moc velký
+Zmenšit v grafickém editoru na maxim
+
+## 
+
+## U varianty nopic/noheader nefungují sdílecí tlačítka
+Přidat do stylů `.m-socials {display: none;}`, tím se tlačítka vedle perexu schovají.
